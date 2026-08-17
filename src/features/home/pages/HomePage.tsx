93436900640443e783ca433ec7957f2b0a1a23f9
boatngin <thanapat.boat16@gmail.com>
2026-08-17 16:56:46 +0700

    x1

--- src/features/home/pages/HomePage.tsx
+++ src/features/home/pages/HomePage.tsx
@@ -1,7 +1,7 @@
-import { PagePlaceholder } from "@/components/common/PagePlaceholder";
-
 export function HomePage() {
-  return <>
-    <h1>gg</h1>
-  </>;
+  return (
+    <>
+      <h1>gg</h1>
+    </>
+  );
 }
