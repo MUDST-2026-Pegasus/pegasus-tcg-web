34bdf979d8e6da73e5efad6c1133bd6c7987193f
NTZ9008 <mathermobile6439@gmail.com>
2026-09-24 21:32:46 +0700

    chore: remove dummy test files

--- cypress/e2e/spec.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('/')
-    cy.contains('Pegasus') // change this to text that actually exists if it fails
-  })
-})
--- src/App.test.tsx
+++
@@ -1,7 +0,0 @@
-import { describe, it, expect } from 'vitest';
-
-describe('App', () => {
-  it('should render successfully', () => {
-    expect(true).toBe(true);
-  });
-});
